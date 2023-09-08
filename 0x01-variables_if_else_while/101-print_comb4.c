c770670ffa3ade040bf29f581a1a4ee000cd28fd
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-08 23:11:50 +0300

    Triple digit combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8 ; i++)
+	{
+		for (j = i + 1 ; j < 9 ; j++)
+		{
+			for (k = j + 1 ; k < 10 ; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
