212e49a9f6e73de34fe4d3f9440e2443d6aa4cbe
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-08 16:43:55 +0300

    Print base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		printf("%d", count);
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
