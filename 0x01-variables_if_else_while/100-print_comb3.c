23812a9fada97a73a30dddd21d76a0815c02d698
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-08 22:47:51 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,17 +12,15 @@ int main(void)
 
 	for (i = 0; i < 9 ; i++)
 	{
-		for (j = 1 ; j < 10 ; j++)
+		for (j = i + 1 ; j < 10 ; j++)
 		{
-			if (i != 1 || j != 1)
-			{
+			
 			putchar('0' + i);
 			putchar('0' + j);
 			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
-			}
 			}
 
 		}
