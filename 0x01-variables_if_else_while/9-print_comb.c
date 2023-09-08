8499c67e0441a2794d86b77bba9a1b3ed28e24ec
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-08 18:00:32 +0300

    Print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count = 0;
+
+	while (count < 10)
+	{
+		putchar('0' + count);
+
+		if (count < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		count++;
+	}
+
+	return (0);
+}
