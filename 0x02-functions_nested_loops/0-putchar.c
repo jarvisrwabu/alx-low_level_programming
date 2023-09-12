2b004a36eb88f7b7504393c616790ea9c87f19a0
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-12 18:10:10 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -14,20 +14,6 @@ return (write(1, &c, 1));
 }
 
 /**
-* _puts - writes a string to stdout
-* @str: The string to print
-*/
-void _puts(char str[])
-{
-int i = 0;
-
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-}
-/**
 * main - Entry point
 *
 *
