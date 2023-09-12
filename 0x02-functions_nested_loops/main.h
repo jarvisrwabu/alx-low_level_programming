522a09a04df47f31b4276fc638f553b5d240437e
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-12 18:37:00 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,6 @@
-#include <unistd.h>
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+ *this file contains header file
+ *
+ */
 
-/**
-* _puts - writes a string to stdout
-* @str: The string to print
-*/
-void _puts(char str[])
-{
-int i = 0;
-
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-}
+int _putchar(char c);
