d1a9f4da6bf89c2f5e1f753dcf8c9679bf7e7429
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-12 23:39:33 +0300

    Absolute of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - returns absolute of a number
+ * @n: The number to get the absolute of
+ *
+ * Return: The absolute of the number
+ *
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
