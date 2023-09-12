99a7a7875d82ab2d167696ed17120d3a754a316d
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-12 18:02:30 +0300

    First task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+* _puts - writes a string to stdout
+* @str: The string to print
+*/
+void _puts(char str[])
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+}
+/**
+* main - Entry point
+*
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char message[] = "_putchar\n";
+_puts(message);
+return (0);
+}
+
+
