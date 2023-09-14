91698f3c6c82a826b04dd663862da6f41fad1c05
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-14 23:06:37 +0300

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar(' ');
+		}
+		else
+		{
+			(j > 9) ? _putchar((j / 10) + '0') : 0;
+			_putchar((j % 10) + '0');
+			_putchar(' ');
+		}
+}
