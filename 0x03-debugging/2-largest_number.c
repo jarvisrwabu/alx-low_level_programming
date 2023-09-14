6ac56c8f5341edd43ff2913ac2f3e588f36b626a
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-15 00:47:49 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	if (b > a && b > c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	if (c > a && c > b)
+	else
 	{
 		largest = c;
 	}
