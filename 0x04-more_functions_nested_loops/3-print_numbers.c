128720d5d8fdb7cad5f6fe32279ac76915303aec
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-14 12:10:25 +0300

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - This function prints numbers from 0 to 9
+ *
+ *
+ * Return: Does not return any value
+ */
+
+void print_numbers(void)
+{
+
+	int i;
+
+	for (i = 1; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
