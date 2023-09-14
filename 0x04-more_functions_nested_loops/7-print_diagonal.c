978bdbc7497c4181bbbf12b7b0d3a85612705e96
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-14 21:38:17 +0300

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - This function prints a straight line n number of times
+ * @n: Number of slashes to print on the screen
+ *
+ * Return: Does not return any value
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
