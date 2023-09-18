4b82051da00de03f45fa078512c98cd40adc4cd3
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-18 23:44:31 +0300

    Add _putchar in main.h and fix more errors

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <string.h>
 
 /**
@@ -12,13 +13,26 @@
 void print_rev(char *s)
 {
 	int i, length = strlen(s);
+	char copy[length + 1];
+
+	strcpy(copy, s);
 	int middle = length / 2;
 	char temp;
 
+	/* The algorithm for reversing a string, */
+/* turns out, strrev() is not a standard function */
+
 	for (i = 0; i < middle; i++)
 	{
-		temp = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = temp;
+		temp = copy[i];
+		copy[i] = copy[length - i - 1];
+		copy[length - i - 1] = temp;
 	}
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(copy[i]);
+	}
+	_putchar('\n');
+
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
