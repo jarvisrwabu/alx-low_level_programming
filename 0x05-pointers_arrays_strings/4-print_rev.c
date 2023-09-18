2521b768f939fcfd15d7aea5e66af1869fd31fab
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-18 23:11:11 +0300

    Fix the function print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,5 +11,14 @@
 
 void print_rev(char *s)
 {
-	strrev(s);	/* using strrev() function to reverse a string */
+	int i, length = strlen(s);
+	int middle = length / 2;
+	char temp;
+
+	for (i = 0; i < middle; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
 }
