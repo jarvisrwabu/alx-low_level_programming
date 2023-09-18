bf9aabbb9c8114812c4029058f5d40a11f0921d4
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-18 18:21:48 +0300

    Update to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function checks if the character is in uppercase
+ *
+ * @n: The pointer to input parameter
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = &n;
+	*n = 98; /* Update value of n to 98 */
+}
