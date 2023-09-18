f0ec035c9ae490fdf6810721d512c86373e2f15c
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-18 20:47:59 +0300

    Print in puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - This function returns the length of a string
+ *
+ * @str: The pointer to the string
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	puts(str);	/* puts() is a standard c function */
+}
