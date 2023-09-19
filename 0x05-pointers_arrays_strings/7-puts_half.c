89f69856283ba39f33f609649aef9da3a5f16558
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-19 05:44:30 +0300

    Fix errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,23 +3,23 @@
 #include <string.h>
 
 /**
- * print_rev - This function reverses a string
+ * puts_half - This function prints half of a string
  *
- * @s: The pointer to the string
+ * @str: The pointer to the string
  *
  * Return: Nothing
  */
 
-void print_rev(char *s)
+void puts_half(char *str)
 {
-	int i, length = strlen(s);
+	int i, length = strlen(str);
 	int middle = length / 2;
 
 	if (length % 2 != 0)
 	{
 		for (i = middle + 1; i < length; i++)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 		}
 		_putchar('\n');
 	}
@@ -28,8 +28,8 @@ void print_rev(char *s)
 	{
 		for (i = 0; i < middle; i++)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 		}
 		_putchar('\n');
-
+	}
 }
