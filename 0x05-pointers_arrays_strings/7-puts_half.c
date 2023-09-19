c3d4a652ba376a547d78423d8a6680bb0331a44f
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-19 06:02:44 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,16 +6,16 @@
  * Return: void
  */
 
-        void puts_half(char *str)
+void puts_half(char *str)
 {
-        int i, n, count = 0;
+	int i, n, count = 0;
 
-        for (i = 0; str[i] != '\0'; i++)
-                count++;
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
 
-        n = (count - 1) / 2;
-        for (i = n + 1; str[i] != '\0'; i++)
-                _putchar(str[i]);
+	n = (count - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
+		_putchar(str[i]);
 
-        _putchar('\n');
+	_putchar('\n');
 }
