c2454da5288c9e828cba2c3afd094d5c9cf7a9ff
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-19 05:58:00 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,16 @@
 
 void print_array(int *a, int n)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 
-        {
-        printf("%d", a[i]);
+	{
+		printf("%d", a[i]);
 
-        if (i < n - 1)
-                printf(", ");
-        }
+		if (i < n - 1)
+			printf(", ");
+	}
 
-        printf("\n");
+	printf("\n");
 }
