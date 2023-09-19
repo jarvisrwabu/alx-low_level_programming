671560765aabc4a0486cceb034e034a86e7f1fe2
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-19 05:21:58 +0300

    Fix betty style errors

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,18 +12,18 @@
 
 void rev_string(char *s)
 {
-        int i;
-        int length = strlen(s);
+	int i;
+	int length = strlen(s);
 	char temp;
 	/* This is the basic algorithm for reversing */
 	/* a string by swapping and using a temp holder */
 
-        for (i = 0; i  < length / 2; i++)
-        {
+	for (i = 0; i  < length / 2; i++)
+	{
 
 		temp = s[i];
 		s[i] = s[length - i - 1];
 		s[length - i - 1] = temp;
-        }
+	}
 }
 
