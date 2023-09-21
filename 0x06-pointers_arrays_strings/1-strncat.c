fc755540c39f6c005f08650132e4c91981b9564c
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-21 23:21:08 +0300

    More concatenation

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncat - concatenate n bytes to destination string
+ *
+ * @dest: string to be appended to
+ *
+ * @src: string to append
+ *
+ * @n: append n number of bytes(chars)
+ *
+ * Return: concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, i;
+	/* Get the length of the source using a standard library */
+
+	length = strlen(dest);
+	/* Concatenate source to the destination */
+	/* with atmost bytes n */
+
+	for (i = 0; src[i] != '\0' && i < n ; i++, length++)
+	{
+		dest[length] = src[i];
+	}
+	/* Terminate the source string */
+	dest[length] = '\0';
+	return (dest);
+}
