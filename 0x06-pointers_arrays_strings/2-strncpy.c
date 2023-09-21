5905534956ec088bfa0a81faa6f0cc6a1482daf6
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-21 23:42:45 +0300

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: string to be copied to
+ *
+ * @src: string to copy
+ *
+ * @n: append n number of bytes(chars)
+ *
+ * Return: concatenated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	/* Copy source to the destination */
+	/* with atmost bytes n */
+
+	for (i = 0; src[i] != '\0' && i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	/* Terminate the new destination string */
+	dest[i] = '\0';
+	return (dest);
+}
