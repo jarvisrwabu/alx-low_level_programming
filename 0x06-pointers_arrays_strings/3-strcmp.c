0d422e99c5a6784a3a7d61e56fa8107751784335
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-09-22 00:25:40 +0300

    Fix errors

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,11 @@
 
 /**
  * _strcmp - compare two strings
+ *
  * @s1: one string
+ *
  * @s2: one string
+ *
  * Return: int that tells num spaces in between
  */
 
@@ -19,21 +22,3 @@ int _strcmp(char *s1, char *s2)
 	else
 		return (0);
 }
-
-/* another method
-
-int _strcmp(char *s1, char *s2)
-{
-        // while neither strings have ended
-        while (*s1 != '\0' || *s2 != '\0')
-        {
-		// if element in arr1 in arr2 are diff, return difference
-                if (*s1 != *s2)
-                        return (*s1 - *s2);
-		// iterate index
-                s1++;
-                s2++;
-        }
-        return (0);
-}
-*/
