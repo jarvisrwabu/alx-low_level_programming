35c693b2432179b0ae310acb3ace984b44d432f3
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-11 00:08:09 +0300

    Print File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	/* Printing name of file it was compiled from */
+	printf("%s\n", __FILE__);
+	return (0);
+}
