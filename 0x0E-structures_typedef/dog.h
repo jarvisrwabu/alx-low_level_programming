ef426ad6985f088b0e79b9add2005688fbbf08d4
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-11 13:25:59 +0300

    Add new Function prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
