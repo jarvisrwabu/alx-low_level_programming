055e4f3b67df11b22737ea44617c1ee477efbe62
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-12 01:04:49 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,18 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL || size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
-	/* iterate through array and call searching function */
+
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(arr[i]))
-		{
+		if (cmp(array[i]))
 			return (i);
-		}
 	}
 	return (-1);
-
 }
