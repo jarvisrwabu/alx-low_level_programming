c0d286b5b14865ba18b235acedaf079f4cd4afa2
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-11 13:21:50 +0300

    Use typedef to change name from struct dog to dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: second member
  * @owner: third member
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
