6cf12d7a88a35c66d3147d6fd960512ba7bdbc66
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-13 01:35:33 +0300

    Fix betty error

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,32 +11,32 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        unsigned int i;
-        /* Define va_list to work with variable arguments */
-        va_list lst;
+	unsigned int i;
+	/* Define va_list to work with variable arguments */
+	va_list lst;
 
-        va_start(lst, n);
+	va_start(lst, n);
 
-        /* Loop through the arguments and print them */
+	/* Loop through the arguments and print them */
 
-        for (i = 0; i < n; i++)
-        {
-                char *str = va_arg(lst, char *);
+	for (i = 0; i < n; i++)
+	{
+		char *str = va_arg(lst, char *);
 
-                if (i < n - 1 && separator != NULL)
-                {
-                        printf("%s%s", str, separator);
-                }
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s%s", str, separator);
+		}
 		else if (str == NULL)
 		{
 			printf("(nil)");
 		}
-                else
-                {
-                        printf("%s", str);
-                }
-        }
-        printf("\n");
+		else
+		{
+			printf("%s", str);
+		}
+	}
+	printf("\n");
 	/* Free memory assigned to va_list */
 	va_end(lst);
 }
