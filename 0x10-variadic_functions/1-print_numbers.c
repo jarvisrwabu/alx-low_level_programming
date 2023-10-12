cbb0c9e6ddf609c0a7bf99e6bad2d4761de7bd61
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-13 00:45:07 +0300

    Print the number of variable arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - returns sum of all parameters
+ * @n: number of arguments
+ * @separator: separator between the numbers
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	/* Define va_list to work with variable arguments */
+	va_list lst;
+
+	va_start(lst, n);
+
+	/* Loop through the arguments and print them */
+
+	for (i = 0; i < n; i++)
+	{
+		int p = va_arg(lst, const unsigned int);
+
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%d%s", p, separator);
+		}
+		else
+		{
+			printf("%d", p);
+		}
+	}
+	/* Free memory allocated for va_lst */
+	va_end(lst);
+}
